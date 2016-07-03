9c48c63ee3759e707340e1af0fafe216018fa961
Leandro henrique Oliveira Fernandes <leandroohf@gmail.com>
2016-07-03 11:11:41 -0700

    Refactor: Add data wrangle module
    * Dev Features engineering
    * Dev Sentiment Analysis
    * Dev narrative score

---
+++ data_wrangler.R
@@ -0,0 +1,28 @@
+##* ****************************************************************
+##  Programer[s]: Leandro Fernandes
+##  Company/Institution:
+##  email: leandroohf@gmail.com
+##  Date: June 18, 2016
+##
+##  The author believes that share code and knowledge is awesome.
+##  Feel free to share and modify this piece of code. But don't be
+##  impolite and remember to cite the author and give him his credits.
+##* ****************************************************************
+
+
+library(stringr)
+library(jsonlite)
+library(feather)
+
+source("./utils/data.R")
+source("./utils/utils.R")
+
+raop.file <- "data/raw/pizza_request_dataset.json"
+raop.df   <- fromJSON( raop.file, flatten=TRUE)
+
+pos.words <- readLines("./dict/positive-words.txt")
+neg.words <- readLines("./dict/negative-words.txt")
+
+raop.target <- BuildDataTarget(raop.df, pos.words, neg.words)
+
+write_feather(raop.target, "data/stage/raop_target.feather")
