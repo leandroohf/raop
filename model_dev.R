9c93fb68ae6b90f0fc8d17f84d6194c31214e340
Leandro henrique Oliveira Fernandes <leandroohf@gmail.com>
2016-07-09 20:07:37 -0700

    refacto: clean code (old comment)

--- model_dev.R
+++ model_dev.R
@@ -34,17 +34,6 @@ print(dim(val.data))
 
 resp.var <- 'requester_received_pizza'
 
-## cols.num <- c("requester_received_pizza",
-##               "requester_account_age_in_days_at_request", 
-##               "requester_number_of_posts_at_request",
-##               "requester_upvotes_minus_downvotes_at_request", 
-##               "nword",
-##               "desire.score", "family.score", "money.score", 
-##               "job.score", "student.score")
-
-## data.exp <- DataExplorer(train.data[,cols.num],resp.var)
-## data.exp$GetCorrDashBoard()
-
 cat("Featuring Selection... \n")
 param.list <- list("objective" = "binary:logistic",
                    "eta" = 0.01,
